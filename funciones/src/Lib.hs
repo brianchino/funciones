c3546cdfd1e2799ad2b753318ec5b0a24db51246
chino brian <bchinoyanarico@frba.utn.edu.ar>
2023-06-06 19:58:43 -0300

    otrocommit

--- funciones/src/Lib.hs
+++ funciones/src/Lib.hs
@@ -1,4 +1,44 @@
+ import Data.List
  {-
+ head [3,2,4]
+ 3
+
+ tail [3,2,4]
+ [2,4]
+
+ last [3,2,4]
+ 4
+
+sort [2,5,1,3]
+[1,2,3,5]
+ 
+["hola","como","estas","felipe"] !! 2
+"estas"
+
+2 : [3,4,5]
+[2,3,4,5]
+
+sum [1,2,3]
+6
+
+concat [[1],[2,3],[4]]
+[1,2,3,4]
+
+maximum [2,3,4,1]
+4
+
+iterate (*2) 1
+[1,2,4,8,16....
+
+repeat 'a'
+"aaaaaaaaaaa...."
+
  replicate 3 "hola"
  ["hola","hola","hola"]
+
+cycle [1,2,3]
+[1,2,3,1,2,3,1,2,3,1,2,3.....]
+
+git config --global user.name "brian chino"
+git config --global user.email bchinoyanarico@frba.utn.edu.ar
  -}
