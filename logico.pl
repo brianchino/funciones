31e26217f1f5198e1f2f86ff81c29e45f207d51c
chino brian <bchinoyanarico@frba.utn.edu.ar>
2023-08-15 20:41:41 -0300

    logico

---
+++ logico.pl
@@ -0,0 +1,44 @@
+/*
+entre valores
+between(min,max,nro)
+
+X is 10 // 3  division entera
+X=3
+X is 10 // 3  division flotante
+X=3.33
+
+
+abs(-8,X).
+X=8
+
+X is sqrt(9)
+X=3.0
+
+append([1,3],[2,4],X). concatenacion
+X=[1,3,2,4]
+
+union([1,3],[3,1,2],X). union
+X=[3,1,2]
+
+intersection([1,3],[1,2],X). interseccion
+X= [1]
+
+
+nth1(indice,lista,elemento)
+
+max_member(X,[2,3]).
+X=3
+min_member
+
+flatten([[],[2],[3,4]],X). aplanar
+X=[2,3,4]
+
+list_to_set([1,2,3,1,3,4],X). sin repetir elementos
+X=[1,2,3,4]
+
+reverse([h,o,l,a],X).
+X = [a, l, o, h].
+
+msort([3,2,4,1],X).  ordenar
+X=[1,2,3,4]
+*/
