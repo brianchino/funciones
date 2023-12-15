0f906d19584ac36ad615b8a06e94c5b3355c403a
chino brian <bchinoyanarico@frba.utn.edu.ar>
2023-12-15 17:52:43 -0300

    modificado

--- funciones/src/Lib.hs
+++ funciones/src/Lib.hs
@@ -1,32 +1,7 @@
  import Data.List
  {-
- head [3,2,4]
- 3
-
- tail [3,2,4]
- [2,4]
-
- last [3,2,4]
- 4
-
-sort [2,5,1,3]
-[1,2,3,5]
- 
-["hola","como","estas","felipe"] !! 2
-"estas"
-
-2 : [3,4,5]
-[2,3,4,5]
-
-sum [1,2,3]
-6
-
-concat [[1],[2,3],[4]]
-[1,2,3,4]
-
-maximum [2,3,4,1]
-4
-
+---------------------------------------------------------------------
+CREAR LISTAS
 iterate (*2) 1
 [1,2,4,8,16....
 
@@ -38,11 +13,86 @@ repeat 'a'
 
 cycle [1,2,3]
 [1,2,3,1,2,3,1,2,3,1,2,3.....]
+----------------------------------------------------------------
+TIPO DE DATOS PROPIOS
+import Text.Show.Functions
+data Investigador = Investigador{
+    nombre      :: String,
+    experiencia :: Int, 
+    companiero  :: Pokemon,
+    mochila     :: [Item],
+    pokemons    :: [Pokemon]
+} deriving (Show)
+
+data Rango = Cielo | Estrella | Constelacion | Galaxia deriving(Show,Eq)
+
+data Pokemon = Pokemon {
+    mote        :: String,
+    descripcion :: String,
+    nivel       :: Int,
+    puntosBase  :: Int
+} deriving (Show)
+--------------------------------------------------------------------
+EJEMPLO DE SUSTRAER UNA FUNCION GENERAL PARA VARIOS PUNTOS
+
+nombreInvestigadoresConMasDe3Alfa :: Expedicion -> [Investigador] -> [String]
+nombreInvestigadoresConMasDe3Alfa = reporte nombre ((>3) . length . filter esAlfa . pokemons)  
 
 
-holadwdwdwddwd12123132
+realizarExpedicion :: Expedicion -> Investigador -> Investigador
+realizarExpedicion expedicion investigador = foldr ($) investigador expedicion 
+
+experienciaInvestigadoresRangoGalaxia :: Expedicion -> [Investigador] -> [Int]
+experienciaInvestigadoresRangoGalaxia = reporte experiencia ((== Galaxia) . rangoDe) 
+
+FUNCION GENERAL
+reporte :: (Investigador -> a) ->  (Investigador -> Bool) -> Expedicion -> [Investigador] -> [a] 
+reporte rasgo condicion expedicion = map rasgo . filter condicion . map (realizarExpedicion expedicion) 
+
+companierosInvestigadoresAlMenosNivel10 :: Expedicion -> [Investigador] -> [Pokemon]
+companierosInvestigadoresAlMenosNivel10 = reporte companiero ((>=10) . nivel . companiero) 
+
+ultimosPokemonsInvestigadoresNivelPar :: Expedicion -> [Investigador] -> [[Pokemon]]
+ultimosPokemonsInvestigadoresNivelPar = reporte (take 3 . reverse . pokemons) (all (even . nivel) . pokemons) 
+-------------------------------------------------------------------------------------------------
+WARIO CON LLAVES FRANCESAS INFINITAS CON PRECIO SUBIENDO DE UNO A UNO
+wario :: Plomero
+wario = Plomero "Wario" (map (Herramienta "llave francesa" Hierro) [1..]) [] 0.5
+-----------------------------------------------------------------------------------
+USAR PATTERN MATCHING DE BUENA FORMA
+esBuena :: Herramienta -> Bool
+esBuena (Herramienta _ Hierro precio)         = precio > 10000
+esBuena (Herramienta "martillo" material _ )  = elem material [Madera,Goma]
+esBuena _                                     = False
+-----------------------------------------------------------------------------
+-- Funciones útiles
+between n m x = elem x [n .. m]
+
+maximoSegun f = foldl1 (mayorSegun f)
+
+mayorSegun f a b
+  | f a > f b = a
+  | otherwise = b
+ ---------------------------------------------------------------------------------- 
+obstaculosSeguidos [] tiro = 0 
+obstaculosSeguidos (x:xs) tiro  
+    | puedeSuperar x tiro = 1 + obstaculosSeguidos xs (x tiro)
+    | otherwise           = 0
+    -------------------------------------------------------------------------------
+    
+deltaSegun :: (a -> Int) -> a -> a -> Int
+deltaSegun f algo1 algo2 = f algo1 - f algo2
+-----------------------------------------------------------------------------
+USAR GIT
+
 git config --global user.name "brian chino" 23132123
 git config --global user.email bchinoyanarico@frba.utn.edu.ar
  
-hola
+cd Desktop 
+git clone
+git commit -m "hola"
+git add .
+git pull
+git push
+
  -}
